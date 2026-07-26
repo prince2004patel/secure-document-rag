57d28e1557ae9411750567071b3e91eab077879c
prince2004patel <princepatel30324@gmail.com>
2026-07-26 10:18:55 +0530

    backend update(app file)

--- app.py
+++ app.py
@@ -3,19 +3,41 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
+from database.db import db
+from security.auth_utils import bcrypt
+
+from routes.auth import auth_bp
+from routes.upload import upload_bp
+from routes.query import query_bp
+
 load_dotenv()
 
 app = Flask(__name__)
+
 CORS(app)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+bcrypt.init_app(app)
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(upload_bp)
+app.register_blueprint(query_bp)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Secure Document Query System Backend Running",
-        "status": "success"
+        "message": "Secure Document Query System Backend Running"
     })
 
 
@@ -23,20 +45,6 @@ def home():
 def health():
     return jsonify({
         "status": "healthy"
-    })
-
-
-@app.route("/upload", methods=["POST"])
-def upload():
-    return jsonify({
-        "message": "Upload endpoint ready"
-    })
-
-
-@app.route("/query", methods=["POST"])
-def query():
-    return jsonify({
-        "message": "Query endpoint ready"
     })
 
 
